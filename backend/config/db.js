803fc39e2235b49056ae5f82afbea4cfb9612419
Gabriel <gjbarret@asu.edu>
2024-10-11 19:25:09 -0600

    Add authentication routes and refactor database configuration
    Integrate user authentication by adding login and registration routes. Refactor database connection setup and rename configuration file from data.js to db.js. Also, update environment variables and add a SECRET_KEY for JWT signing.

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 
-const connectDB = async () => {
+const connectDb = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI);
         console.log('Connected to MongoDB');
     } catch {
-        console.log('MongoDB connection error...');
+        console.log('MongoDB connection error');
     }
 }
 
-module.exports = connectDB;
+module.exports = connectDb;
--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,19 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const authRoutes = require('./routes/authRoute');
 
 
 const port = process.env.PORT || 5001;
-const connectDB = require('./config/data');
+const connectDB = require('./config/db');
 connectDB();
-
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use('/api/auth', authRoutes);
 
 
 
-app.listen(port, () => {
-    console.log(`Server started on port ${port}`);
-})
+
+app.listen(port, () => console.log(`Listening on port ${port}`));

395db8999aa35477210b6cbe802d79e6aefb97bf
Gabriel <gjbarret@asu.edu>
2024-11-07 10:27:43 -0700

    .env configuration
    changed name temporarily to .env.local which caused an error so it was changed back to .env.
    add "*.env" in the gitirnore file

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -1,5 +1,5 @@
 import React, { createContext, useState, useEffect } from 'react';
-import jwtDecode from 'jwt-decode'; // Corrected import
+import { jwtDecode } from 'jwt-decode';
 
 export const AuthContext = createContext();
 
